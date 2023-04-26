33d051dff0eea5c512953f5eb28a51582bc72cb6
Krit <tanakritkhunkhae@gmail.com>
2023-04-27 01:01:45 +0700

    first day

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: const MyCounter(),
+    );
+  }
+}
+
+class MyCounter extends StatefulWidget {
+  const MyCounter({super.key});
+
+  @override
+  State<MyCounter> createState() => _MyCounterState();
+}
+
+class _MyCounterState extends State<MyCounter> {
+  
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My First Page"),
+      ),
+      body: Container(
+          alignment: Alignment.center,
+          color: Colors.green,
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [const Text('This is body'), Text(counter.toString())],
+          )),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        setState(() {
+          counter++;
+        });
+        print(counter);
+      }),
+    );
+  }
+}
